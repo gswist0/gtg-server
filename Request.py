9abc5e903c3b55771545fc2c74066902e09ba40d
gswist0 <grzswist@gmail.com>
2026-07-13 16:29:08 +0200

    comment

--- Request.py
+++ Request.py
@@ -5,6 +5,6 @@ class Request:
     game_id: str = ""
     action: str ="" #guess, next, ability, skip, start
     guessed_game: str = ""
-    ability_used: str = ""
+    ability_used: str = "" #shield, unlock, skip_round, extra_life
     infinite: bool = False#for starting a new game
     
